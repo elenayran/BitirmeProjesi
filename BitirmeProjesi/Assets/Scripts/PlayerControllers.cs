aaea146161cc1e33891c5617d7b411b9b6fcbdce
elenayran <36763284+elenayran@users.noreply.github.com>
2021-02-22 22:45:25 +0300

    playercontroller

---
+++ BitirmeProjesi/Assets/Scripts/PlayerControllers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllers : MonoBehaviour
+{
+    private float mySpeedX;
+    [SerializeField] float speed;
+    private Rigidbody2D myBody;
+    private Vector3 defaultLocalScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        defaultLocalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        mySpeedX = Input.GetAxis("Horizontal");
+       myBody.velocity = new Vector2(mySpeedX  * speed, myBody.velocity.y);
+
+        if (mySpeedX>0)
+        {
+            transform.localScale = new Vector3(defaultLocalScale.x, defaultLocalScale.y, defaultLocalScale.z);
+        }
+        else if (mySpeedX<0)
+        {
+            transform.localScale = new Vector3(-defaultLocalScale.x, defaultLocalScale.y, defaultLocalScale.z);
+        }
+    }
+}
